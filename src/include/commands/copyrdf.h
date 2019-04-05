5fb7fbe4b8c5fa9383fe9ddfcea7c2e3e7b08e1f
ChrisLpk <48610501+ChrisLpk@users.noreply.github.com>
2019-04-05 14:05:07 +0800

    Add files via upload

---
+++ src/include/commands/copyrdf.h
@@ -0,0 +1,18 @@
+#ifndef COPYRDF_H
+#define COPYRDF_H
+
+
+
+#include "nodes/execnodes.h"
+#include "nodes/parsenodes.h"
+#include "parser/parse_node.h"
+#include "tcop/dest.h"
+
+
+
+
+extern void DoCopyRdf(CopyRdfStmt *stmt);
+
+
+
+#endif							/* COPYRDF_H */
